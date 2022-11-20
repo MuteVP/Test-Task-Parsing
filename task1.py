772eb264eeee2cbfa81814f2e717dcee6a1c5d4d
MuteVP <parilov.ivan-parilov@yandex.ru>
2022-11-20 15:57:21 +0700

    Исправление замечаний
    Исправлено использование xpath и selenium

--- task1.py
+++ task1.py
@@ -2,84 +2,83 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.action_chains import ActionChains
-from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from fake_useragent import UserAgent
 from selenium.webdriver.common.keys import Keys
 import time
 
-
-# Подготовка к запросу
 options = Options()
 options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
 ua = UserAgent()
-userAgent = ua.random   # использование случайного user agent
+userAgent = ua.random
+
 print(" Start...\n")
 options.add_argument(f'user-agent={userAgent}')
 s = Service('C:\Program Files\chromedriver.exe')
-driver = webdriver.Chrome(options = options, service=s)
-driver.delete_all_cookies()     # сайт блокрует доступ после первого соединения, анализируя переданные cookie. Удаление cookie перед каждым запросом решает проблему с Access Denied
+driver = webdriver.Chrome(options=options, service=s)
+driver.delete_all_cookies()     #в данной задаче лучшего решения для обхода защиты нет;
+# любая подмена cookie сопряжена с возникающими ошибками;
+# сохранять же куки нет никакой необходимости;
+# данный способ удаляет все куки в текущей сессии и выполняет поставленную задачу
+
 driver.get('https://www.nseindia.com/')
 
-# в этом задании поиск удобнее проводить через XPATH
-# значения time.sleep не везде критичны и выстанавливались для наглядности отображения и прогрузки элементов сайта
-menu_marketData = driver.find_element(By.XPATH,"//a[@id='link_2']")
+menu_marketData = driver.find_element(By.CSS_SELECTOR, "#link_2")
 time.sleep(0.1)
 hover = ActionChains(driver).move_to_element(menu_marketData)
 hover.perform()
-menu_marketData_preOpenMarket = driver.find_element(By.XPATH,"//a[@href='/market-data/pre-open-market-cm-and-emerge-market']")
+
+menu_marketData_preOpenMarket = driver.find_element(By.CSS_SELECTOR,
+                                                    "a[href='/market-data/pre-open-market-cm-and-emerge-market']")
 time.sleep(0.1)
 driver.delete_all_cookies()
 menu_marketData_preOpenMarket.click()
 time.sleep(3)
-final_prices = driver.find_elements(By.XPATH, "//td[@class='bold text-right']")
+final_prices = driver.find_elements(By.CSS_SELECTOR, "td[class='bold text-right']")
 prices = [final_price.get_attribute("innerHTML") for final_price in final_prices]
 
-names_tags = driver.find_elements(By.XPATH, "//td/a[@target='_blank']")
+names_tags = driver.find_elements(By.CSS_SELECTOR, "td a[target='_blank']")
 names = [name.get_attribute("innerHTML") for name in names_tags]
 
-for i in range(0,len(names)):   # оказалось, что некоторые имена парсятся со знаком ';'. При выводе в .csv это критично, поэтому в цикле происходит удаление этих символов
+for i in range(0, len(names)):
     if names[i].find(';') != -1:
-        names[i] = names[i][:names[i].find(';')] + names[i][names[i].find(';')+1:]
+        names[i] = names[i][:names[i].find(';')] + names[i][names[i].find(';') + 1:]
 
 file = open('prices.csv', 'w')
-for i in range(0,len(prices)):
-    file.write(names[i]+";"+prices[i]+"\n")
+for i in range(0, len(prices)):
+    file.write(names[i] + ";" + prices[i] + "\n")
 
 file.close()
 
-#  ===пользовательский сценарий===
-# сценарий взят из примера в задании
-# задержки выставлены для наглядности и лучшей прогрузки страниц
+# ===пользовательский сценарий===
 
 driver.delete_all_cookies()
-driver.find_element(By.XPATH, "//img[@title='NSE India, National Stock Exchange']").click()
+driver.find_element(By.CSS_SELECTOR, "img[title='NSE India, National Stock Exchange']").click()
 time.sleep(3)
-graph = driver.find_element(By.XPATH, "//div[@role='tablist']")
+graph = driver.find_element(By.CSS_SELECTOR, "div[role='tablist']")
 graph.location_once_scrolled_into_view
 
 time.sleep(3)
-driver.find_element(By.XPATH, "//a[@id='tabList_NIFTYBANK']").click()
+driver.find_element(By.CSS_SELECTOR, "a[id='tabList_NIFTYBANK']").click()
 time.sleep(3)
-
 driver.execute_script("window.scrollBy(0, 100);")
 time.sleep(2)
 driver.delete_all_cookies()
-driver.find_element(By.XPATH, "//*[@id='tab4_gainers_loosers']/div[3]/a").click()
+# driver.find_element(By.XPATH,"//*[@id='tab4_gainers_loosers']/div[3]/a").click()
+driver.find_element(By.CSS_SELECTOR, "a[href='/market-data/live-equity-market?symbol=NIFTY BANK']").click()
 
 time.sleep(3)
 
 driver.delete_all_cookies()
-driver.find_element(By.XPATH, "//option[@value='NIFTY ALPHA 50']").click()
+driver.find_element(By.CSS_SELECTOR, "option[value='NIFTY ALPHA 50']").click()
 
-time.sleep(3)
+time.sleep(10)
 
-try:    # при переключении выбора из списка таблица прогружается очень плохо, поэтому при невозможности скролла таблицы, производится скролл в конец веб-страницы
-    table = driver.find_element(By.XPATH,"//table[@id='equityStockTable']").send_keys(Keys.END)
+try:
+    table = driver.find_element(By.CSS_SELECTOR, "table[id='equityStockTable']").send_keys(Keys.END)
 
 except:
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
 
 time.sleep(3)
 print(" End.\n Results locate in prices.csv")
--- task2.py
+++ task2.py
@@ -1,95 +1,62 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
-import time
+from requests_html import HTML, HTMLSession
 
+session = HTMLSession()
 
-options = Options()
-options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
-s = Service('C:\Program Files\chromedriver.exe')
-driver = webdriver.Chrome(options = options, service=s)
-driver.get('https://twitter.com/elonmusk/')
+r = session.get('https://www.twitter.com/elonmusk/')  # HTTP запрос
 
-tweets_count = 10
-tweets_txt = []
-tweets_txt_for_output = []
-number = 0
-number_links = 0
+a = 1
 links_to_comm = []
-while number < tweets_count:
 
-    if number > 0:
-        driver.execute_script("window.scrollBy(0, 100);")
-        time.sleep(1)
+r.html.render(scrolldown=4, sleep=5, timeout=20)
+for i in range(0, 10):
+    # в данном случае использование CSS-селекторов будет связано с ошибками при поиске и огромным количеством
+    # дополнительных лишних данных. Использование xpath решает эту проблему, а также гарантирует отсутствие
+    # ошибочных данных
+    links_to_comm.append(str(r.html.xpath(
+        '/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/section/div/div/div[' + str(
+            i + 1) + ']/div/div/div/article/div/div/div/div[2]/div[2]/div[1]/div/div/div[1]/div/div/div[2]/div/div[3]/a')[
+                                 0].absolute_links)[2:-2])
 
-        
-    html = driver.page_source
-    soup = BeautifulSoup(html,'lxml')
-    tweets = soup.find_all('article', class_='css-1dbjc4n r-1loqt21 r-18u37iz r-1ny4l3l r-1udh08x r-1qhn6m8 r-i023vh r-o7ynqc r-6416eg')
+print(links_to_comm)
 
-    for tweet in tweets:
-        
-        tmp = tweet
-        tweet = tweet.find('div',class_= 'css-901oao r-18jsvk2 r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0')
-        
-        try:
-            if tweet.text not in tweets_txt:
-                tweets_txt.append(tweet.text)
-                tweets_txt_for_output.append(tweet.text)
-                number = number + 1
+twit_text_only = []
+norm_links = []
+for i in range(0, 10):
+    r = session.get(links_to_comm[i])
+    r.html.render(scrolldown=2, sleep=5, timeout=20)
+    twits_text = r.html.find('article[class="css-1dbjc4n r-18u37iz r-1ny4l3l r-1udh08x r-1qhn6m8 r-i023vh"]',
+                             first=True)
 
-        except AttributeError:
-            if tmp.text not in tweets_txt:
-                tweets_txt.append(tmp.text)
-                tweets_txt_for_output.append("***картинка или видео***")
-                number = number + 1
-                
-        if number >= tweets_count:
-            break
-
-    elems = driver.find_elements(By.CSS_SELECTOR, ".css-1dbjc4n [href]")
-    links = [elem.get_attribute('href') for elem in elems]
-    for link in links:
-        
-        if str(link).find('/status/') != -1 and str(link).find('/photo/') == -1 and (link not in links_to_comm):
-            links_to_comm.append(link)
-            
-            number_links+=1
-        if number_links >= 10:
-            break
+    twit_text = str(twits_text.text).split('\n')
 
 
-users_links = []
-for link in links_to_comm:
-    driver.get(link)
-    time.sleep(2)
-    driver.execute_script("window.scrollBy(0, 300);")
-    time.sleep(2)
-    html = driver.page_source
-    soup = BeautifulSoup(html,'lxml')
-    comments = soup.find_all('div', class_='css-901oao css-1hf3ou5 r-14j79pv r-18u37iz r-1qd0xha r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-qvutc0')#('article', class_= 'css-1dbjc4n r-1loqt21 r-18u37iz r-1ut4w64 r-1ny4l3l r-1udh08x r-1qhn6m8 r-i023vh r-o7ynqc r-6416eg')
-    #comments = soup.find_all('div', class_='css-1dbjc4n r-1awozwy r-18u37iz r-1wbh5a2 r-dnmrzs r-1ny4l3l')
-    num = 0
+    if len(twit_text) > 2:
+        twit_text_only.append(twit_text[2])
+    else:
+        twit_text_only.append('Twit is a picture or unavailable')
+
+
+    commentators_links = r.html.find('article')
+    n = 0
     tmp = []
+    for links in commentators_links:
+        if n > 0:
+            links_1 = links.absolute_links
+            for link in links_1:
+                if str(link).find('/status/') == -1 and str(link).find('/elonmusk') == -1 and str(link).find(
+                        '/photo/') == -1 and str(link).find('help.twitter') == -1 and str(link).find(
+                        '/twitter.com/') != -1 and str(link).find(
+                        '/t.co/') == -1 and link not in tmp:
+                    # print(link)
+                    tmp.append(link)
+        n += 1
+    norm_links.append(tmp)
 
-    for comment in comments:
-        if comment.text != '@elonmusk':
-            tmp.append('https://twitter.com/' + comment.text[1:])
 
-            num = num + 1
+for i in range(0, 10):
+    print(i + 1, twit_text_only[i])
+    print()
+    print(norm_links[i][0], '\n', norm_links[i][1], '\n', norm_links[i][2])
+    print()
+    print()
 
-    users_links.append(tmp)
-
-
-print("\n\n   === Последние 10 твитов Илона Маска ===\n\n")
-print("============================================")
-for i in range(0,10):
-    print(i+1, ' - ', tweets_txt_for_output[i])
-    print("\n    Последние комменаторы:")
-    print("   ", users_links[i][0], "\n   ", users_links[i][1],"\n   " ,users_links[i][2])
-    print("============================================")
-          
-
-driver.quit()
